8b5d2f46ed4e09931ea2c3b449563eb73e75e84a
Adrian van den Houten <adrian@zailab.com>
2018-07-09 00:53:33 +0200

    test(master): got everything to work

--- src/main/java/com/scholarcoder/chat/server/Server.java
+++ src/main/java/com/scholarcoder/chat/server/Server.java
@@ -1,80 +1,98 @@
 package com.scholarcoder.chat.server;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Server {
-    final int port;
+    private ServerSocket serverSocket;
     private Thread serverThread;
     private ExecutorService connectionHandlerExecutorService;
-    private List<Future> connectionFutures;
+    private List<Future> clientHandlerConnections;
 
+    private volatile boolean started = false;
+    private int port;
 
     public Server(int port) {
         this.port = port;
-        this.connectionHandlerExecutorService = Executors.newCachedThreadPool();
-        this.connectionFutures = new ArrayList<>();
+        connectionHandlerExecutorService = Executors.newCachedThreadPool();
+        clientHandlerConnections = new ArrayList<>();
     }
 
     public void start() {
+        System.out.println("Starting server...");
         serverThread = new Thread(() -> {
             try {
-                ServerSocket serverSocket = new ServerSocket(port);
+                serverSocket = new ServerSocket();
+                serverSocket.setReuseAddress(true);
+                serverSocket.bind(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), port));
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+
+            if(!Thread.currentThread().isInterrupted()) {
                 System.out.println("Server is listening...");
-                while(!Thread.currentThread().isInterrupted()) {
-                    try {
-                        Socket socket = serverSocket.accept();
-                        System.out.println("Handling new connection");
-                        Future clientHandlerTask = connectionHandlerExecutorService.submit(() -> {
-                            ClientHandler client = new ClientHandler(socket, generateSessionId(), this);
-                            client.handle();
-                        });
-                        connectionFutures.add(clientHandlerTask);
+            }
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+            started = true;
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    Socket socket = serverSocket.accept();
+
+                    System.out.println("Handling new connection");
+                    Future submit = connectionHandlerExecutorService.submit(() -> {
+                        ClientHandler client = new ClientHandler(socket, generatedId(), this);
+                        client.handle();
+                    });
+                    clientHandlerConnections.add(submit);
+
                 }
+                catch(SocketException e) {
+                    return;
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
                 serverSocket.close();
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
         });
         serverThread.start();
+
+        while(!started) { }
     }
 
-    protected String generateSessionId() {
-        Random random = new Random();
-        int randomInt = random.nextInt(999);
-
-        NumberFormat numberFormat = new DecimalFormat("#000");
-        return numberFormat.format(randomInt);
+    protected String generatedId() {
+        return UUID.randomUUID().toString();
     }
 
     public void stop() {
+        System.out.println("Stopping server");
         serverThread.interrupt();
-        connectionFutures.forEach(future -> future.cancel(true));
+
+        clientHandlerConnections.forEach(future -> future.cancel(true));
         connectionHandlerExecutorService.shutdown();
 
-    }
+        try {
+            if(serverSocket!=null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    private void tryCloseServerSocket() {
-//        try {
-//            System.out.println("Closing server socket");
-//            serverSocket.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        System.out.println("Server shutdown");
     }
 }
--- src/test/java/com/scholarcoder/chat/server/ServerTest.java
+++ src/test/java/com/scholarcoder/chat/server/ServerTest.java
@@ -1,17 +1,72 @@
 package com.scholarcoder.chat.server;
 
+import com.scholarcoder.chat.server.user.UserRepositorySingleton;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ServerTest {
 
+    private static String HOST = "localhost";
+    private static int PORT = 34567;
+
+    Server server;
+
+    @Before
+    public void setUp() {
+        server = new Server(PORT);
+        server.start();
+
+        UserRepositorySingleton.get().deleteAll();
+    }
+
+    @After
+    public void tearDown() {
+        server.stop();
+    }
+
     @Test
-    public void testRandomNumberGenerator() {
-        String regexMatcherForRandomNumber = "\\d{3}";
-        Server server = new Server(0);
+    public void testRegisterUser() {
+        String command = "REG adrian";
+        String expectedResponse = "201 Created";
+        Client client = new Client(HOST, PORT);
 
-        String randomSessionId = server.generateSessionId();
+        System.out.println("Sending command");
+        String response = client.sendCommand(command);
+        System.out.println("Finished sending command");
 
-        Assert.assertTrue(randomSessionId.matches(regexMatcherForRandomNumber));
+        Assert.assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    public void testRegisterUserThatAlreadyExists() {
+        String command = "REG adrian";
+        String expectedResponse = "409 Conflict";
+        Client client = new Client(HOST, PORT);
+
+
+        System.out.println("Sending command");
+        client.sendCommand(command);
+        String response = client.sendCommand(command);
+        System.out.println("Finished sending command");
+
+        Assert.assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    public void testListRegisteredUsers() {
+        Client client = new Client(HOST, PORT);
+        client.sendCommand("REG adrian");
+        client.sendCommand("REG josie");
+
+        String command = "LISTUSER";
+        String expectedResponse = "adrian,josie";
+
+        System.out.println("Sending command");
+        String response = client.sendCommand(command);
+        System.out.println("Finished sending command");
+
+        Assert.assertEquals(expectedResponse, response);
     }
 }
