f2834aa955521f7a5ce908670f9bb587ca0f8269
Adrian van den Houten <adrian@zailab.com>
2018-07-09 01:04:50 +0200

    test(master): refactored server class

--- src/main/java/com/scholarcoder/chat/server/Server.java
+++ src/main/java/com/scholarcoder/chat/server/Server.java
@@ -2,11 +2,8 @@ package com.scholarcoder.chat.server;
 
 import java.io.IOException;
 import java.net.*;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -30,31 +27,16 @@ public class Server {
     public void start() {
         System.out.println("Starting server...");
         serverThread = new Thread(() -> {
-            try {
-                serverSocket = new ServerSocket();
-                serverSocket.setReuseAddress(true);
-                serverSocket.bind(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), port));
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (!tryBindServerSocket()) {
                 return;
             }
-
             if(!Thread.currentThread().isInterrupted()) {
                 System.out.println("Server is listening...");
             }
-
-            started = true;
+            notifyServerStarted();
             while (!Thread.currentThread().isInterrupted()) {
                 try {
-                    Socket socket = serverSocket.accept();
-
-                    System.out.println("Handling new connection");
-                    Future submit = connectionHandlerExecutorService.submit(() -> {
-                        ClientHandler client = new ClientHandler(socket, generatedId(), this);
-                        client.handle();
-                    });
-                    clientHandlerConnections.add(submit);
-
+                    tryHandleConnection();
                 }
                 catch(SocketException e) {
                     return;
@@ -63,15 +45,47 @@ public class Server {
                     e.printStackTrace();
                 }
             }
-            try {
-                serverSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         });
         serverThread.start();
+        waitUntilStarted();
+    }
 
-        while(!started) { }
+    private void tryHandleConnection() throws IOException {
+        Socket socket = serverSocket.accept();
+
+        System.out.println("Handling new connection");
+        Future submit = connectionHandlerExecutorService.submit(() -> {
+            ClientHandler client = new ClientHandler(socket, generatedId(), this);
+            client.handle();
+        });
+        clientHandlerConnections.add(submit);
+    }
+
+    private boolean tryBindServerSocket() {
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.setReuseAddress(true);
+            serverSocket.bind(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), port));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private synchronized void notifyServerStarted() {
+        started = true;
+        notify();
+    }
+    private synchronized void waitUntilStarted() {
+        while(!started) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     protected String generatedId() {
