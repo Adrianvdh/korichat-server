306e72b4ec5344d67d80a793dd588e48eae93c4a
Adrian van den Houten <adrian@zailab.com>
2018-07-17 00:21:27 +0200

    test(user-management): cleaned up some tests

--- src/test/java/com/scholarcoder/chat/server/integration/ServerUserManagementTest.java
+++ src/test/java/com/scholarcoder/chat/server/integration/ServerUserManagementTest.java
@@ -1,28 +1,48 @@
 package com.scholarcoder.chat.server.integration;
 
 import com.scholarcoder.chat.server.Client;
+import com.scholarcoder.chat.server.transport.ChatResponse;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.UUID;
 
 public class ServerUserManagementTest extends AbstractServerTest {
 
     @Test
     public void testRegisterUser() {
-        String command = "REG adrian CHAT/1.0";
-        String expectedResponse = "201 Created";
         Client client = new Client(HOST, PORT);
+        String expectedResponse = "CHAT/1.0 201 Created";
 
+        String command = "REG adrian CHAT/1.0";
         String response = client.sendCommand(command);
 
         Assert.assertEquals(expectedResponse, response);
     }
 
+    @Ignore
+    @Test
+    public void testUseUser_expectSessionResponseLooksLikeAppropriateResponse() {
+        Client client = new Client(HOST, PORT);
+        client.sendCommand("REG adrian CHAT/1.0");
+
+        String command = "CHAT/1.0 USE adrian";
+
+        String expectedResponseRegex = "CHAT/1.0 200 OK\n"
+                + "SESSIONID: " + uuidRegexMatcher();
+
+        String actualResponse = client.sendCommand(command);
+
+        Assert.assertTrue(actualResponse.matches(expectedResponseRegex));
+    }
+
+    @Ignore
     @Test
     public void testRegisterUserThatAlreadyExists() {
         String command = "REG adrian CHAT/1.0";
-        String expectedResponse = "409 Conflict";
+        String expectedResponse = "CHAT/1.0 409 Conflict";
         Client client = new Client(HOST, PORT);
-
 
         client.sendCommand(command);
         String response = client.sendCommand(command);
@@ -30,18 +50,8 @@ public class ServerUserManagementTest extends AbstractServerTest {
         Assert.assertEquals(expectedResponse, response);
     }
 
-    @Test
-    public void testListRegisteredUsers() {
-        Client client = new Client(HOST, PORT);
-        client.sendCommand("REG adrian CHAT/1.0");
-        client.sendCommand("REG josie CHAT/1.0");
 
-        String command = "LISTUSER";
-        String expectedResponse = "adrian,josie";
-
-        String response = client.sendCommand(command);
-
-        Assert.assertEquals(expectedResponse, response);
+    private String uuidRegexMatcher() {
+        return "[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}";
     }
-
 }
